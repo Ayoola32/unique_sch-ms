1c03b5d582e2cfd06debaafa6e13de2bd19cdbba
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-02-01 20:19:17 +0000

    script file added in a seperate assets folder

--- assets/js/login_script.js
+++
@@ -1,9 +0,0 @@
-// Toggle password visibility
-const togglePassword = document.getElementById('togglePassword');
-const password = document.getElementById('password');
-
-togglePassword.addEventListener('click', () => {
-    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
-    password.setAttribute('type', type);
-    togglePassword.classList.toggle('bi-eye-slash-fill');
-});
