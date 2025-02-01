576cb394ba4c8b80cc448b03fb7a95a033c6325b
Abdulganiyu Abubakar Sidiq <abubakarsidiq994@gmail.com>
2025-02-01 02:17:19 +0000

    login script file created, added password toggle visibility code for the login

---
+++ assets/js/login_script.js
@@ -0,0 +1,9 @@
+// Toggle password visibility
+const togglePassword = document.getElementById('togglePassword');
+const password = document.getElementById('password');
+
+togglePassword.addEventListener('click', () => {
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    togglePassword.classList.toggle('bi-eye-slash-fill');
+});
